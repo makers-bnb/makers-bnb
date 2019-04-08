3ae585daa9381a7596807fb51c6fad3d7857dee6
Paul Martin <communty.hivemind@gmail.com>
2019-04-08 14:12:11 +0100

    User can enter account details on sign-up page

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ class MakersBnB < Sinatra::Base
     erb :index
   end
 
+  post '/user' do
+    # in here is where the user gets created
+    redirect '/somewhere'
+  end
+
+  get '/somewhere' do
+    "New account created"
+  end
+
   run! if app_file == $0
 end
-
