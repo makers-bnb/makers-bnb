6cd0ebbb624aa3d0daea8423626236d59e0d5ca5
Paul Martin <communty.hivemind@gmail.com>
2019-04-08 15:10:07 +0100

    Update destination route after sign-up

--- app.rb
+++ app.rb
@@ -12,10 +12,10 @@ class MakersBnB < Sinatra::Base
     user = User.create(email: params[:email],
                        password: params[:password])
     session[:user_id] = user.id
-    redirect '/somewhere'
+    redirect '/spaces'
   end
 
-  get '/somewhere' do
+  get '/spaces' do
     user = User.get(session[:user_id])
     "New account created with email: #{user.email}"
   end
