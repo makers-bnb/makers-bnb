15b96bb650b3692c0af567d9437dc7a76cded5bc
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-09 16:15:49 +0100

    ADD: User can confirm and reject requests against their spaces
    Confirm and Reject buttons added to each request against current user's spaces. These post to /spaces/:req_id route and update the status of the request in the request db appropriately

--- spec/features/confirm_booking_spec.rb
+++ spec/features/confirm_booking_spec.rb
@@ -11,10 +11,30 @@ feature 'Confirm booking' do
     expect(page).to have_content('2019-04-09')
     expect(page).to have_content('secondUser@gmail.com')
   end
-  # scenario 'I click confirm next to a request and see status changed to confirmed' do
-  #
-  # end
-  # scenario 'I click reject next to a request and see status changed to rejected' do
-  #
-  # end
+  scenario 'I click accept next to a request and see status changed to confirmed' do
+    user_sign_up('firstUser@gmail.com', '123')
+    add_space('My Place', 'Amazing flat', '50')
+    user_sign_up('secondUser@gmail.com', '123')
+    make_request('2019-04-09')
+    user_log_in('firstUser@gmail.com', '123')
+    visit '/requests'
+    click_button 'Accept Booking'
+    expect(page).to have_content('Confirmed')
+    expect(page).to have_content('My Place')
+    expect(page).to have_content('2019-04-09')
+    expect(page).to have_content('secondUser@gmail.com')
+  end
+  scenario 'I click reject next to a request and see status changed to rejected' do
+    user_sign_up('firstUser@gmail.com', '123')
+    add_space('My Place', 'Amazing flat', '50')
+    user_sign_up('secondUser@gmail.com', '123')
+    make_request('2019-04-09')
+    user_log_in('firstUser@gmail.com', '123')
+    visit '/requests'
+    click_button 'Reject Booking'
+    expect(page).to have_content('Rejected')
+    expect(page).to have_content('My Place')
+    expect(page).to have_content('2019-04-09')
+    expect(page).to have_content('secondUser@gmail.com')
+  end
 end
