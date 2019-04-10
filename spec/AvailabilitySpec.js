b60d29c569243891ec231dfb89ee07e725efe6ad
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-10 15:54:51 +0100

    ADD: add javascript tests and code to test available dates
    Looking to split a string, which contains dates, and convert that string into a date,
    will return true if date is available and false if it is not. Also formatting.

--- spec/AvailabilitySpec.js
+++ spec/AvailabilitySpec.js
@@ -3,13 +3,31 @@ describe("Availability", function() {
   var song;
 
   beforeEach(function() {
-    availability = new Availability();
+    availability = new Availability('2019-04-21 2019-03-01');
   });
 
   describe('dates to array', function() {
     it("should convert a strig of dates to an array", function() {
-      var dates = availability.datesToArray('2019-04-21 2019-03-01');
+      var dates = Availability.datesToArray('2019-04-21 2019-03-01');
       expect(dates).toEqual(['2019-04-21', '2019-03-01']);
+    });
+  });
+
+  describe('convert string to a formatted date', function () {
+    it("should convert a date object to a string", function () {
+      var date = new Date('Wed Apr 10 2019 06:00:00 GMT+0100');
+      expect(Availability.dateConversion(date)).toEqual('2019-04-10');
+    });
+  });
+
+  describe('date to array', function () {
+    it("should return true if date is not array", function () {
+      var date = new Date('Wed Apr 10 2019 06:00:00 GMT+0100');
+      expect(availability.dateAvailable(date)).toEqual(true);
+    });
+    it("should return false if date is in array", function () {
+      var date = new Date('Wed Apr 21 2019 06:00:00 GMT+0100');
+      expect(availability.dateAvailable(date)).toEqual(false);
     });
   });
 });
--- src/Availability.js
+++ src/Availability.js
@@ -1,6 +1,17 @@
-function Availability() {
+function Availability(datesString) {
+  this.datesString = datesString;
 };
 
-Availability.prototype.datesToArray = function(datesString) {
-  return datesString.split(' ')
+Availability.datesToArray = function(datesString) {
+  return datesString.split(' ');
+};
+
+Availability.dateConversion = function(date) {
+  return date.toISOString().substring(0, 10);
+};
+
+Availability.prototype.dateAvailable = function(date) {
+  var dateString = Availability.dateConversion(date);
+  var datesArray = Availability.datesToArray(this.datesString)
+  return !datesArray.includes(dateString);
 };
