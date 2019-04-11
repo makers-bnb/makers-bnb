7c1ede9a2edaac87ca5ee86009dce880c9af6294
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-11 14:16:17 +0100

    REFACTOR: to make the rest route to logout RESTful and CSS work with the menu
    Making the logout route RESTFUL and working with the CSS to make the logout button
    appear to be a link, with some CSS styling, as without the styling the menu item
    appears as a button that doesnt look appealing when compared to spaces and request
    link items.

--- spec/features/no_bookings_outside_range_spec.rb
+++ spec/features/no_bookings_outside_range_spec.rb
@@ -3,7 +3,7 @@ feature 'Request a booking' do
     user_sign_up('johnnylandlord@landlords.com', 'moneymoneymoney')
     add_space("Bob's Bunker", 'My bunker is amazing', '50', '2019-04-20', '2019-04-29')
     user_sign_up('billyrenter@renters.com', 'love2rent')
-    click_link 'Log out'
+    click_button 'Log out'
     user_sign_up('renter@renter', 'password')
     click_link 'Request to book'
     page.should have_selector('#request_date_picker[data-availability-start="2019-04-20"]')
