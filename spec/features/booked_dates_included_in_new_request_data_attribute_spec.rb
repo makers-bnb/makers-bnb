51f03c5c2ca9a6720dd65eade7115ceef2a09a43
Paul Martin <communty.hivemind@gmail.com>
2019-04-11 16:29:22 +0100

    Resolve broken tests after merge conflict

--- spec/features/booked_dates_included_in_new_request_data_attribute_spec.rb
+++ spec/features/booked_dates_included_in_new_request_data_attribute_spec.rb
@@ -2,13 +2,13 @@ feature 'Booked dates are unavailable' do
   scenario "Dates which are already booked should appear in a data attribute on the date picker" do
     user_sign_up('landlord@landlord', 'password')
     add_space('My flat', 'Is a very nice flat', '99', '2020-01-01', '2020-12-31')
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter@renter', 'password')
     make_request('2020-05-03')
     make_request('2020-06-03')
     make_request('2020-07-03')
-    click_link('Log out')
+    click_button('Log out')
 
     user_log_in('landlord@landlord', 'password')
     click_link('Requests')
@@ -20,7 +20,7 @@ feature 'Booked dates are unavailable' do
         .ancestor('.request')
         .find('button', text: 'Accept booking')
         .click
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter2@renter', 'password')
     click_link('Request to book')
@@ -30,13 +30,13 @@ feature 'Booked dates are unavailable' do
   scenario "Pending requests are not included" do
     user_sign_up('landlord@landlord', 'password')
     add_space('My flat', 'Is a very nice flat', '99', '2020-01-01', '2020-12-31')
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter@renter', 'password')
     make_request('2020-05-03')
     make_request('2020-06-03')
     make_request('2020-07-03')
-    click_link('Log out')
+    click_button('Log out')
 
     user_log_in('landlord@landlord', 'password')
     click_link('Requests')
@@ -44,7 +44,7 @@ feature 'Booked dates are unavailable' do
         .ancestor('.request')
         .find('button', text: 'Accept booking')
         .click
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter2@renter', 'password')
     click_link('Request to book')
@@ -54,13 +54,13 @@ feature 'Booked dates are unavailable' do
   scenario "Rejected requests are not included" do
     user_sign_up('landlord@landlord', 'password')
     add_space('My flat', 'Is a very nice flat', '99', '2020-01-01', '2020-12-31')
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter@renter', 'password')
     make_request('2020-05-03')
     make_request('2020-06-03')
     make_request('2020-07-03')
-    click_link('Log out')
+    click_button('Log out')
 
     user_log_in('landlord@landlord', 'password')
     click_link('Requests')
@@ -68,7 +68,7 @@ feature 'Booked dates are unavailable' do
         .ancestor('.request')
         .find('button', text: 'Reject booking')
         .click
-    click_link('Log out')
+    click_button('Log out')
 
     user_log_in('landlord@landlord', 'password')
     click_link('Requests')
@@ -76,7 +76,7 @@ feature 'Booked dates are unavailable' do
         .ancestor('.request')
         .find('button', text: 'Accept booking')
         .click
-    click_link('Log out')
+    click_button('Log out')
 
     user_sign_up('renter2@renter', 'password')
     click_link('Request to book')
