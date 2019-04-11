e69fa828fe62d4dcc3f6b64ee85cd36f59bc498a
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-11 11:07:03 +0100

    ADD: Updated interface to pass min and max dates into datepicker
    Used javascript in our interface file to extract the start and end dates from the html and then pass these into datepicker

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-  var datesString = $("#request_date_picker").attr("data-booked-dates")
-  var availability = new Availability(datesString);
+  var bookedDatesString = $("#request_date_picker").attr("data-booked-dates")
+  var availabilityStart = $("#request_date_picker").attr("data-availability-start")
+  var availabilityEnd = $("#request_date_picker").attr("data-availability-end")
+  var availability = new Availability(bookedDatesString);
   var availability_bound = availability.dateAvailable.bind(availability)
 
-  $('#request_date_picker')
+
   $('#request_date_picker').datepicker({
-    minDate: new Date(2019, 0, 1),
-    maxDate: new Date(2019, 5, 31),
+    minDate: new Date(availabilityStart),
+    maxDate: new Date(availabilityEnd),
     dateFormat: 'yy-mm-dd',
     constrainInput: true,
-    beforeShowDay: availability_bound
+    beforeShowDay: availability_bound,
+    showAnim: 'fade'
   });
 });
